e59ebb0cf7d6888ad10992a8c72a15c429415ca4
Devendra Singh Sachan <dsachan@andrew.cmu.edu>
2018-01-13 00:38:17 -0500

    Improved preprocessing code

--- preprocess.py
+++ preprocess.py
@@ -3,21 +3,33 @@ from __future__ import unicode_literals
 import collections
 import io
 import re
-import numpy
+import six
+import numpy as np
 import progressbar
+import json
+import os
+from collections import namedtuple
+from config import get_args
 
 
 split_pattern = re.compile(r'([.,!?"\':;)(])')
 digit_pattern = re.compile(r'\d')
 
+Special_Seq = namedtuple('Special_Seq', ['PAD', 'EOS', 'UNK', 'BOS'])
+Vocab_Pad = Special_Seq(PAD=0, EOS=1, UNK=2, BOS=3)
 
-def split_sentence(s):
-    s = s.lower()
-    s = s.replace('\u2019', "'")
-    s = digit_pattern.sub('0', s)
+
+def split_sentence(s, tok=False):
+    if tok:
+        s = s.lower()
+        s = s.replace('\u2019', "'")
+        s = digit_pattern.sub('0', s)
     words = []
     for word in s.strip().split():
-        words.extend(split_pattern.split(word))
+        if tok:
+            words.extend(split_pattern.split(word))
+        else:
+            words.append(word)
     words = [w for w in words if w]
     return words
 
@@ -31,18 +43,18 @@ def count_lines(path):
         return sum([1 for _ in f])
 
 
-def read_file(path):
+def read_file(path, tok=False):
     n_lines = count_lines(path)
     bar = progressbar.ProgressBar()
     with open_file(path) as f:
         for line in bar(f, max_value=n_lines):
-            words = split_sentence(line)
+            words = split_sentence(line, tok)
             yield words
 
 
-def count_words(path, max_vocab_size=40000):
+def count_words(path, max_vocab_size=40000, tok=False):
     counts = collections.Counter()
-    for words in read_file(path):
+    for words in read_file(path, tok):
         for word in words:
             counts[word] += 1
 
@@ -50,22 +62,79 @@ def count_words(path, max_vocab_size=40000):
     return vocab
 
 
-def make_dataset(path, vocab):
-    word_id = {word: index for index, word in enumerate(vocab)}
+def make_dataset(path, vocab, tok=False):
+    w2id = {word: index for index, word in enumerate(vocab)}
     dataset = []
     token_count = 0
     unknown_count = 0
-    for words in read_file(path):
-        array = make_array(word_id, words)
+    for words in read_file(path, tok):
+        array = make_array(w2id, words)
         dataset.append(array)
         token_count += array.size
-        unknown_count += (array == 1).sum()
+        unknown_count += (array == Vocab_Pad.UNK).sum()
     print('# of tokens: %d' % token_count)
-    print('# of unknown: %d (%.2f %%)'
-          % (unknown_count, 100. * unknown_count / token_count))
+    print('# of unknown: %d (%.2f %%)' % (unknown_count, 100. * unknown_count / token_count))
     return dataset
 
 
 def make_array(word_id, words):
-    ids = [word_id.get(word, 1) for word in words]
-    return numpy.array(ids, 'i')
+    ids = [word_id.get(word, Vocab_Pad.UNK) for word in words]
+    return np.array(ids, 'i')
+
+
+if __name__ == "__main__":
+    args = get_args()
+
+    print(json.dumps(args.__dict__, indent=4))
+
+    # Vocab Construction
+    source_path = os.path.join(args.input, args.source_train)
+    source_vocab = ['<pad>', '<eos>', '<unk>', '<bos>'] + count_words(source_path, args.source_vocab, args.tok)
+    target_path = os.path.join(args.input, args.target_train)
+    target_vocab = ['<pad>', '<eos>', '<unk>', '<bos>'] + count_words(target_path, args.target_vocab, args.tok)
+
+    # Train Dataset
+    source_data = make_dataset(source_path, source_vocab, args.tok)
+    target_data = make_dataset(target_path, target_vocab, args.tok)
+    assert len(source_data) == len(target_data)
+    train_data = [(s, t) for s, t in six.moves.zip(source_data, target_data) if 0 < len(s) < 50 and 0 < len(t) < 50]
+
+    # Display corpus statistics
+    print("Source Vocab: {}".format(len(source_vocab)))
+    print("Target Vocab: {}".format(len(target_vocab)))
+    print('Original training data size: %d' % len(source_data))
+    print('Filtered training data size: %d' % len(train_data))
+
+    # Valid Dataset
+    source_path = os.path.join(args.input, args.source_valid)
+    source_data = make_dataset(source_path, source_vocab, args.tok)
+    target_path = os.path.join(args.input, args.target_valid)
+    target_data = make_dataset(target_path, target_vocab, args.tok)
+    assert len(source_data) == len(target_data)
+    valid_data = [(s, t) for s, t in six.moves.zip(source_data, target_data) if 0 < len(s) and 0 < len(t)]
+
+    # Test Dataset
+    source_path = os.path.join(args.input, args.source_test)
+    source_data = make_dataset(source_path, source_vocab, args.tok)
+    target_path = os.path.join(args.input, args.target_test)
+    target_data = make_dataset(target_path, target_vocab, args.tok)
+    assert len(source_data) == len(target_data)
+    test_data = [(s, t) for s, t in six.moves.zip(source_data, target_data) if 0 < len(s) and 0 < len(t)]
+
+    source_w2id = {word: index for index, word in enumerate(source_vocab)}
+    target_w2id = {word: index for index, word in enumerate(target_vocab)}
+
+    target_id2w = {i: w for w, i in target_w2id.items()}
+    source_id2w = {i: w for w, i in source_w2id.items()}
+
+    # Save the dataset to numpy files
+    np.save(os.path.join(args.input, 'train.npy'), train_data)
+    np.save(os.path.join(args.input, 'valid.npy'), valid_data)
+    np.save(os.path.join(args.input, 'test.npy'), test_data)
+
+    # Save the vocab in json
+    with io.open(os.path.join(args.input, 'vocab.src.json'), 'w', encoding='utf-8') as f:
+        json.dump(source_id2w, f, sort_keys=True, indent=2)
+
+    with io.open(os.path.join(args.input, 'vocab.trg.json'), 'w', encoding='utf-8') as f:
+        json.dump(target_id2w, f, sort_keys=True, indent=2)
