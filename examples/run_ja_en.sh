1d46b42abec4e7080119aa3b882a6c6ba39fedc2
Devendra Singh Sachan <dsachan@andrew.cmu.edu>
2018-02-02 22:55:52 +0530

    updated ja->en example

--- examples/run_ja_en.sh
+++ examples/run_ja_en.sh
@@ -1,3 +1,5 @@
+cd ..
+
 # Preprocess
 python preprocess.py -i data/ja_en -s-train train-big.ja -t-train train-big.en -s-valid dev.ja -t-valid dev.en -s-test test.ja -t-test test.en --save_data demo
 
