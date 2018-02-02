06978196b556da1530dd4c4bc55ce99b7b837b0d
Devendra Singh Sachan <dsachan@andrew.cmu.edu>
2018-02-02 23:07:19 +0530

    updated ja->en example

--- examples/run_ja_en.sh
+++ examples/run_ja_en.sh
@@ -1,7 +1,9 @@
+AIAYN="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
 cd ..
 
 # Preprocess
-python preprocess.py -i data/ja_en -s-train train-big.ja -t-train train-big.en -s-valid dev.ja -t-valid dev.en -s-test test.ja -t-test test.en --save_data demo
+python ${AIAYN}/preprocess.py -i data/ja_en -s-train train-big.ja -t-train train-big.en -s-valid dev.ja -t-valid dev.en -s-test test.ja -t-test test.en --save_data demo
 
 # Train
-python train.py -i data/ja_en --data demo --batchsize 128 --tied --beam 5 --dropout 0.2 --epoch 40 --layer 1 --multi_heads 8 --gpu 0
+python ${AIAYN}/train.py -i data/ja_en --data demo --batchsize 128 --tied --beam 5 --dropout 0.2 --epoch 40 --layer 1 --multi_heads 8 --gpu 0
