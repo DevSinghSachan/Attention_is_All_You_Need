be4d9891f8b5ec22e5349d9e8ea1ba5b321c7f50
Devendra Singh <devendra.singh@petuum.com>
2018-03-19 21:24:40 -0400

    fixed minor bugs

--- tools/bpe_pipeline_en_vi.sh
+++ tools/bpe_pipeline_en_vi.sh
@@ -42,8 +42,8 @@ apply_bpe -c $OUT/data/bpe-codes.${BPE_OPS} < $VALID_SRC > $OUT/data/valid.src
 apply_bpe -c $OUT/data/bpe-codes.${BPE_OPS} < $TEST_SRC > $OUT/data/test.src
 
 echo "Applying BPE on target"
-apply_bpe.py -c $OUT/data/bpe-codes.${BPE_OPS} <  $TRAIN_TGT > $OUT/data/train.tgt
-apply_bpe.py -c $OUT/data/bpe-codes.${BPE_OPS} <  $VALID_TGT > $OUT/data/valid.tgt
+apply_bpe -c $OUT/data/bpe-codes.${BPE_OPS} <  $TRAIN_TGT > $OUT/data/train.tgt
+apply_bpe -c $OUT/data/bpe-codes.${BPE_OPS} <  $VALID_TGT > $OUT/data/valid.tgt
 # We dont touch the test References, No BPE on them!
 cp $TEST_TGT $OUT/data/test.tgt
 
@@ -94,4 +94,5 @@ echo "Step 4b: Evaluate Dev"
 perl $TF/tools/multi-bleu.perl $OUT/data/valid.tgt < $OUT/test/valid.out > $OUT/test/valid.tc.bleu
 perl $TF/tools/multi-bleu.perl -lc $OUT/data/valid.tgt < $OUT/test/valid.out > $OUT/test/valid.lc.bleu
 
-#===== EXPERIMENT END ======
+
+t2t-bleu --translation=$OUT/data/test.tgt --reference=$OUT/test/test.out
